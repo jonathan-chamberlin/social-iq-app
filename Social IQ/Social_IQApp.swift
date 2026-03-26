d13ef58b0863404ef571d7e7262e2bb70567c3f3
Jonathan Chamberlin <jcham17x@gmail.com>
2026-03-26 11:03:50 -0400

    Initial Commit

---
+++ Social IQ/Social_IQApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_IQApp.swift
+//  Social IQ
+//
+//  Created by Jonathan Chamberlin on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Social_IQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
